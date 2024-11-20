7e9c6e2fa2c3edb99b0d23b4adad0ea60c235af7
Greg Bizup <63376055+gbiz123@users.noreply.github.com>
2024-11-20 15:49:46 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -260,8 +260,7 @@ vim.keymap.set('n', 'gr', function() vim.lsp.buf.rename() end)
 vim.keymap.set('n', 'gca', function() vim.lsp.buf.code_action() end)
 vim.keymap.set('n', 'g/', function() vim.cmd('BLines') end)
 vim.keymap.set('n', '<C-f>', function() vim.cmd('Files') end)
-vim.keymap.set('n', '<C-g>', function() vim.cmd('Jumps') end)
-vim.keymap.set('n', 'gb', function() vim.cmd('Lines') end)
+vim.keymap.set('n', '<C-g>', function() vim.cmd('Lines') end)
 
 
 
