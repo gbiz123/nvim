494fac0b49354e22c67bdd8f958f2a1cb7031689
Greg Bizup <63376055+gbiz123@users.noreply.github.com>
2025-02-11 06:05:11 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,21 +1,16 @@
-
-
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-	{
+	 {
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" }
-	},
-	 'karb94/neoscroll.nvim',
-	 'sphamba/smear-cursor.nvim',
+	 },
+     'junegunn/fzf',
+	 'junegunn/fzf.vim', --requires fzf.vim and fzf installed
 	 'mattn/emmet-vim',
-	 'ggandor/leap.nvim',
 	 'norcalli/nvim-colorizer.lua',
 	 'EdenEast/nightfox.nvim',
-	 'junegunn/fzf',
-	 'junegunn/fzf.vim', --requires fzf.vim and fzf installed
 	 'iamcco/markdown-preview.nvim',
      'sho-87/kanagawa-paper.nvim',
 	 'mfussenegger/nvim-jdtls',
@@ -24,6 +19,7 @@ require("lazy").setup({
 	 'neovim/nvim-lspconfig',
      'nvim-tree/nvim-web-devicons',
 	 'hrsh7th/nvim-cmp',
+	 'hrsh7th/cmp-path',
 	 'hrsh7th/cmp-nvim-lsp',
 	 'hrsh7th/cmp-nvim-lsp-signature-help',
 	 'williamboman/mason.nvim',
@@ -105,6 +101,9 @@ require('toggleterm').setup({
 -- colorscheme
 vim.o.termguicolors = true
 
+-- scroll off (keep cursor away from the very top)
+vim.o.scrolloff = 5
+
 -- Default options
 require('nightfox').setup({
   options = {
@@ -115,15 +114,6 @@ require('nightfox').setup({
     terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
     dim_inactive = true,    -- Non focused panes set to alternative background
     module_default = true,   -- Default enable value for modules
-    colorblind = {
-      enable = false,        -- Enable colorblind support
-      simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
-      severity = {
-        protan = 0,          -- Severity [0,1] for protan (red)
-        deutan = 0,          -- Severity [0,1] for deutan (green)
-        tritan = 0,          -- Severity [0,1] for tritan (blue)
-      },
-    },
     styles = {               -- Style to be applied to different syntax groups
       comments = "italic",     -- Value is any valid attr-list value `:help attr-list`
       conditionals = "italic",
@@ -164,10 +154,9 @@ require('nightfox').setup({
   groups = {},
 })
 
--- setup must be called before loading
-vim.cmd("colorscheme terafox")
 
-vim.o.background = 'dark'
+-- setup must be called before loading
+vim.cmd("colorscheme nightfox")
 
 -- relative and absolute line numbers
 vim.wo.number = true
@@ -212,7 +201,7 @@ vim.o.cursorline = true
 require('nvim-autopairs').setup({})
 
 -- nvim tree
-require("nvim-tree").setup()
+require("nvim-tree").setup({view = { width = 50 }})
 
 -- LSP STUFF BELOW
 -- mason
@@ -226,25 +215,14 @@ cmp.setup({
 		documentation = cmp.config.window.bordered()
 	},
 	mapping = cmp.mapping.preset.insert({
-      ['<C-p>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-n>'] = cmp.mapping.scroll_docs(4),
+      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-d>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       ['<TAB>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
-	sorting = {
-		priority_weight = 2,
-		comparators = {
-			cmp.config.compare.locality,
-			cmp.config.compare.recently_used,
-			cmp.config.compare.exact,
-			cmp.config.compare.kind,
-			cmp.config.compare.score,
-			cmp.config.compare.order
-		}
-	},
 	sources = cmp.config.sources({
+		{ name = 'path' },
 		{ name = 'nvim_lsp' },
 		{ name = 'nvim_lsp_signature_help' },
 	}, {
@@ -252,20 +230,11 @@ cmp.setup({
 	}),
 })
 
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- lsp
--- require('lspconfig').jdtls.setup({ 
--- 	capabilities = capabilities,
--- 	-- Enable signature help: https://github.com/mfussenegger/nvim-jdtls/issues/88
--- 	on_init = function(client)
--- 	  client.server_capabilities.semanticTokensProvider = nil -- Disable lsp highlighting 
--- 	  if client.config.settings then
--- 		client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
--- 	  end
--- 	end
--- })
 require('lspconfig').basedpyright.setup({ capabilities = capabilities })
 require('lspconfig').lua_ls.setup({ capabilities = capabilities })
 require('lspconfig').html.setup({ capabilities = capabilities })
@@ -273,21 +242,25 @@ require'lspconfig'.ts_ls.setup {}
 
 -- key map
 vim.keymap.set('n', '<C-n>', function() vim.cmd('NvimTreeToggle') end)
-vim.keymap.set('n', 'K', function() vim.diagnostic.open_float() end)
-vim.keymap.set('n', 'H', function() vim.lsp.buf.signature_help() end)
+vim.keymap.set('n', 'H', function() vim.diagnostic.open_float() end)
+vim.keymap.set('n', 'S', function() vim.lsp.buf.signature_help() end)
 vim.keymap.set('n', 'gt', function() vim.lsp.buf.type_definition() end)
 vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end)
-vim.keymap.set('n', 'gre', function() vim.lsp.buf.references() end)
-vim.keymap.set('n', 'grn', function() vim.lsp.buf.rename() end)
+vim.keymap.set('n', 'gre', function() vim.lsp.buf.rename() end)
+vim.keymap.set('n', 'grn', function() vim.lsp.buf.references() end)
 vim.keymap.set('n', 'gca', function() vim.lsp.buf.code_action() end)
+
+-- disable arrow keys... your pinky will thank you
+vim.keymap.set('n', '<Down>', function() end)
+vim.keymap.set('n', '<Right>', function() end)
+vim.keymap.set('n', '<Left>', function() end)
+vim.keymap.set('n', '<Up>', function() end)
+
+-- fzf (fuzzy findor)
+-- https://github.com/junegunn/fzf.vim/blob/master/plugin/fzf.vim
 vim.keymap.set('n', 'g/', function() vim.cmd('BLines') end)
 vim.keymap.set('n', '<C-f>', function() vim.cmd('call fzf#vim#files("", fzf#vim#with_preview(), 1)') end)
--- https://github.com/junegunn/fzf.vim/blob/master/plugin/fzf.vim
 vim.keymap.set('n', '<C-g>', function() vim.cmd('call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case -- ".fzf#shellescape(""), fzf#vim#with_preview(), 1)') end)
-
-
-require('leap').create_default_mappings()
-
 
 
 -- treesitter
@@ -323,38 +296,8 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-require('neoscroll').setup({
-  mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
-    '<C-u>', '<C-d>',
-    '<C-b>', '<C-f>',
-    '<C-y>', '<C-e>',
-    'zt', 'zz', 'zb',
-  },
-  hide_cursor = true,          -- Hide cursor while scrolling
-  stop_eof = false,             -- Stop at <EOF> when scrolling downwards
-  respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  duration_multiplier = .5,    -- Global duration multiplier
-  easing = 'linear',           -- Default easing function
-  pre_hook = nil,              -- Function to run before the scrolling animation starts
-  post_hook = nil,             -- Function to run after the scrolling animation ends
-  performance_mode = false,    -- Disable "Performance Mode" on all buffers.
-  ignored_events = {           -- Events ignored while scrolling
-      'WinScrolled', 'CursorMoved'
-  },
-})
-
-require('smear_cursor').setup({
-    cursor_color = '#d3cdc3',
-	stiffness = 0.8,
-	trailing_stiffness = 0.6,      -- 0.3      [0, 1]
-    trailing_exponent = 0,         -- 0.1      >= 0
-    distance_stop_animating = 0.5, -- 0.1      > 0
-    hide_target_hack = false, 
-})
-
+-- harpoon
 local harpoon = require("harpoon")
 harpoon:setup()
-
 vim.keymap.set("n", "ga", function() harpoon:list():add() end)
 vim.keymap.set("n", "gh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
